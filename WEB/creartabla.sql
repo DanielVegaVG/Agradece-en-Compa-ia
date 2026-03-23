2bde4080c89639b70bf605c4a15db3b40ec57ea0
DanielVegaVG <danielvegagarcia.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 08:47:24 +0100

    Add files via upload

---
+++ WEB/creartabla.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Alumnos (
+    puesto CHAR(2) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    usuario VARCHAR(30) NOT NULL,
+    contraseña VARCHAR(20) NOT NULL,
+    jesuita VARCHAR(60) NOT NULL,
+    imagen VARCHAR(30) NOT NULL,
+    nombreWeb VARCHAR(40) NOT NULL,
+    frase VARCHAR(100),
+    PRIMARY KEY (puesto),
+    CONSTRAINT usuario UNIQUE (usuario),
+    CONSTRAINT nombreWeb UNIQUE (nombreWeb),
+    CONSTRAINT magen UNIQUE (imagen)
+);
+////
+INSERT INTO jesuitas (puesto, nombre, usuario, contraseña, jesuita, imagen, nombreWeb, frase) 
+VALUES 
+('01', 'Francisco Javier', 'fjavier', 'pass123', 'San Francisco Javier', 'fjavier.jpg', 'web_fjavier', 'Todo para mayor gloria de Dios'),
+('02', 'Ignacio de Loyola', 'iloyola', 'pass456', 'San Ignacio de Loyola', 'iloyola.jpg', 'web_ignacio', 'En todo amar y servir'),
+('03', 'Pedro Arrupe', 'parrupe', 'pass789', 'Pedro Arrupe', 'parrupe.jpg', 'web_arrupe', 'Caer y levantarse');
