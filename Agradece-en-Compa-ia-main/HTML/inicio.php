2830f0b61f7e63b399e62fe577823318521d4895
DanielVegaVG <danielvegagarcia.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 21:24:04 +0100

    Subida Arch

--- Agradece-en-Compa-ia-main/HTML/inicio.php
+++ Agradece-en-Compa-ia-main/HTML/inicio.php
@@ -1,17 +1,32 @@
 <?php
-
+require 'configdb.php';
+  function conectar(){
+	//Crear la variable de conexion con la base de datos
+	$conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+	$conexion->set_charset("utf8"); 
+    return $conexion;
+  }
 
 $usuario=$_POST["usuario"];
 $contrasena=$_POST["contrasena"];
 
 
-///Primero tenemos que conectar
+session_start();
+$conexion=conectar();
+$sql="select * from Alumnos";
+$_SESSION['id']=$fila["puesto"]
+if($resultado->num_rows > 0){
+    $fila=$resultado->fetch_array()
 
-$sql='SELECT puesto FROM Alumnos
-    WHERE usuario="' .$usuario. '"
-    AND contrasena="' .$contrasena. '";';
 
 
-echo $sql;
-echo '<br/>';
+header('Location: home.php');
+exit();
+}
+else{
+	header('Location: inicioSesionFallo.php');
+	exit();
+}
 
+$conexion->close();
+session_destroy();
