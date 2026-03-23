05beb43c0f29627b25d8cd8b6ea67d79d532608b
DanielVegaVG <danielvegagarcia.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 20:29:15 +0100

    Update configdb.php

--- Agradece-en-Compa-ia-main/BBDD/configdb.php
+++ Agradece-en-Compa-ia-main/BBDD/configdb.php
@@ -1,6 +1,6 @@
 <?php
-define("SERVIDOR",'localhost');
-define("USUARIO",'root');
-define("PASSWORD",'');
-define("BBDD",'proyecto');
+define("SERVIDOR",'23.1daw.esvirgua.com');
+define("USUARIO",'user1daw_23');
+define("PASSWORD",'7nq)xW[X$C;$qt;2');
+define("BBDD",'user1daw_BD1-23');
 ?>
